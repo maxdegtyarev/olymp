79664eaafd10721c6b2c1f457ee39d8e5392aba6
= <=>
2016-10-28 00:24:13 +0300

    Added a algorithm for strings

---
+++ countinstring/zadacha/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class app
+{
+	static void Main()
+	{
+		//Алгоритм неэффективен по памяти, но если тут динамически вводить, то норм)
+		string input = "hello";
+		char[] text = "mynameismaximhelloimfinehello".ToCharArray();
+
+		//Длина входной строки
+		var leng = (input.ToCharArray()).Length;
+		int result = 0;
+		string dline;
+
+		for (int i = 0; i < text.Length - 4; i++) {
+			//Создаём временную переменную для хранения временной строки
+
+			dline = string.Empty;
+			for (int z = i; z < leng; z++) {
+				dline = dline + text[z];
+			}
+			if (input == dline) {
+				result++;
+			}
+			leng++;
+		}
+
+		Console.WriteLine (result);
+		Console.ReadKey ();
+
+
+	
+	}
+
+}
